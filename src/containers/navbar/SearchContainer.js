4fcd5378f3ff88182b8bdf01e11c8464ca5e6493
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-01 20:46:47 -0400

    Adjust colors, layout, and styling.

--- src/containers/navbar/SearchContainer.js
+++ src/containers/navbar/SearchContainer.js
@@ -4,7 +4,10 @@ import { useState } from 'react';
 
 const Wrapper = styled.div`
   display:flex;
+  flex-basis: 0 1 auto;
+  position: relative;
   justify-content: center;
+  align-items: center;
   flex 0 1 auto;
   height: 28px;
   min-width: 125px;
@@ -14,11 +17,39 @@ const Wrapper = styled.div`
 const Input = styled.input`
   width: 100%;
   height: 100%;
-  color: #dbdbdb;
+  color: #8e8e8e;
+  position: absolute;
   border-radius: 3px;
   border: solid 1px #dbdbdb;
   background: #fafafa;
   font-size: 14px
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: -4px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 15px;
+  box-sizing: border-box;
+`
+
+const DefaultText = styled.span`
+  display: inline-block;
+  max-width: 140px;
+  display: inline-block;
+  color: #8e8e8e;
+  font-size: 14px;
+`
+
+const SearchIcon = styled.i`
+  color: #8e8e8e;
+  display: inline-block;
+  font-size: 14px;
+  padding: 0 4px 0 0
 `
 
 const SearchContainer = () => {
@@ -31,8 +62,11 @@ const SearchContainer = () => {
 
   return(
     <Wrapper className="search-container">
-      <Input onChange={handleQueryChange} defaultValue="Search" value={query} ></Input>
-      <div></div>
+      <Input onChange={handleQueryChange} value={query} ></Input>
+      <IconWrapper>
+        <SearchIcon className="bi bi-search"></SearchIcon>
+        <DefaultText>Search</DefaultText>
+      </IconWrapper>
     </Wrapper>
   )
 
