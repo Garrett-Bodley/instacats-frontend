217bc18ec4c5e89f7b3af3a9a6621b22004a28f9
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-13 13:59:17 -0400

    Adjust font-size and add vertical-align: middle

--- src/components/icons/SearchIcon.js
+++ src/components/icons/SearchIcon.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 const SearchIcon = styled.i`
 color: #8e8e8e;
 display: inline-block;
-font-size: 14px;
-padding: 0 4px 0 0
+font-size: 10px;
+padding: 0 4px 0 0;
+vertical-align: middle;
 `
 
 export default SearchIcon
