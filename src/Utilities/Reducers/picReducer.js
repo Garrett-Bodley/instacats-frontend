7874b9b1f8fb177b0b6b2a217843bddc74243ec9
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-30 10:55:41 -0400

    Build out 'ADD_PICS' case.

--- src/Utilities/Reducers/picReducer.js
+++ src/Utilities/Reducers/picReducer.js
@@ -1,5 +1,19 @@
-export const picReducer = (state = {pics: []}, action) => {
+export const picReducer = (
+  state = {
+    pics: [], 
+    dict: {}
+  }, action) => {
   switch(action.type){
+    case 'ADD_PICS':
+      const newDict = Object.assign({}, state.dict)
+      const uniquePics = action.payload.reduce((accumulator, pic) => {
+        if(!newDict[pic.imgur_id]){
+          newDict[pic.imgur_id] = true;
+          accumulator.push(pic)
+        }
+        return accumulator
+      }, [])
+      return  {pics: [...state.pics, ...uniquePics], dict: newDict}
     default: 
       return state
   }
