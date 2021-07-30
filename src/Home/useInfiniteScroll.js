41d867ba4debc17fcc02a79be16e1ea771f7915a
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-30 10:54:47 -0400

    Stub out an infinite scroll hook.

---
+++ src/Home/useInfiniteScroll.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+
+const useInfiniteScroll = (pageNum) => {
+
+  useEffect(() => {
+    fetch()
+  }, [pageNum])
+
+}
+
+export default useInfiniteScroll
