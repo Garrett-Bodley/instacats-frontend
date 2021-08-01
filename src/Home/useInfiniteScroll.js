c47398c908769134e298b1461f88857fa16fcb4c
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-08-01 17:04:06 -0400

    Create an infinite scrolling hook.

--- src/Home/useInfiniteScroll.js
+++ src/Home/useInfiniteScroll.js
@@ -1,11 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState, useCallback, useRef, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPicsByPageNum } from '../Utilities/Actions/picActions'
 
-const useInfiniteScroll = (pageNum) => {
+const useInfiniteScroll = () => {
+
+  const loading = useSelector(state => state.loading)
+  const [pageNum, setPageNum] = useState(1)
+
+  const dispatch = useDispatch()
+  const observer = useRef()
+
+  const lastCardRef = useCallback(node => {
+    if(loading) return;
+    if(observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver( (entries) => {
+      if(entries[0].isIntersecting){ setPageNum(prevNum => prevNum + 1) }
+    });
+    if(node) observer.current.observe(node)
+  }, [loading])
 
   useEffect(() => {
-    fetch()
-  }, [pageNum])
+    dispatch(getPicsByPageNum(pageNum));
+  }, [pageNum, dispatch])
 
+  return { lastCardRef, loading }
 }
 
 export default useInfiniteScroll
