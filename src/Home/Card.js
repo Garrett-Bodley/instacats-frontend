405dd42149d0b2ba9eaca732c2dd564ddf96f111
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-08-31 14:34:08 -0400

    Implement prettier formatting.

--- src/Home/Card.js
+++ src/Home/Card.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
-import Content from './Content'
-import Avatar from '../Icons/Avatar'
+import React from "react";
+import styled from "styled-components";
+import Content from "./Content";
+import Avatar from "../Icons/Avatar";
 
 const Article = styled.article`
   margin-bottom: 24px;
-  border: 1px solid rgb(219, 219, 219)
+  border: 1px solid rgb(219, 219, 219);
   border-radius: 3px;
   background-color: rgb(255, 255, 255);
   color: rgb(38, 38, 38);
@@ -22,27 +22,27 @@ const Header = styled.header`
   border-radius: 3px 3px 0px 0px;
   font-size: 14px;
   font-weight: 600;
-`
+`;
 
 const UsernameContainer = styled.div`
   font-size: 14px;
   font-weight: 600;
   padding-left: 15px;
-`
+`;
+
+const IconBar = styled.section``;
 
 const Card = React.forwardRef((props, ref) => {
-  
   return (
     <Article ref={ref}>
       <Header>
-        <Avatar/>
-        <UsernameContainer>
-          {props.pic.user}
-        </UsernameContainer>
+        <Avatar />
+        <UsernameContainer>{props.pic.user}</UsernameContainer>
       </Header>
       <Content pic={props.pic} />
+      Section
     </Article>
-  )
-})
+  );
+});
 
-export default Card
+export default Card;
