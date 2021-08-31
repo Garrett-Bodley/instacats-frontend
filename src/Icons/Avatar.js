3a61dbb23e8fc53f769b9613392483bb24d5995a
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-08-31 14:41:23 -0400

    Create Avatar component using PersonCircle styled-icon.

--- src/Icons/Avatar.js
+++ src/Icons/Avatar.js
@@ -1,20 +1,18 @@
-import styled from 'styled-components'
-import { PersonCircle } from '@styled-icons/bootstrap'
-
-const Div = styled.div`
-`
+import styled from "styled-components";
+import { PersonCircle } from "@styled-icons/bootstrap";
 
 const Avi = styled(PersonCircle)`
   color: #dbdbdb;
   height: 32px;
   width: 32px;
-`
-const Avatar = () => {
-  return(
-    <Div>
-      <Avi/>
-    </Div>
-  )
-}
+`;
 
-export default Avatar
+const Avatar = () => {
+  return (
+    <>
+      <Avi />
+    </>
+  );
+};
+
+export default Avatar;
