df561b9c2f79db32918833fe317a149e2ac48b50
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-09-03 15:59:45 -0400

    Create component to conditionally render Likes message.

---
+++ src/Home/LikesSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  text-align: left;
+  font-size: 14px;
+  font-weight: 400;
+  padding: 0px 16px 0px 16px;
+  margin-bottom: 8px;
+`;
+
+const Bold = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+`
+
+const LikesSection = ({ likes }) => {
+  const renderMessage = (likes) => {
+    if(!likes) return null;
+    if(likes.length === 0){
+      return null
+    }else if(likes.length === 1){
+      return (
+        <span>
+          Liked by <Bold>{`${likes[0].user.username}`}</Bold>
+        </span>)
+    }else if(likes.length > 1){
+      return (
+        <span>
+          Liked by <Bold>{`${likes[0].user.username}`}</Bold> and <Bold>others</Bold>
+        </span>
+      )
+    }
+  }
+
+  return (
+  <Section>
+    {renderMessage(likes)}
+  </Section>
+  );
+};
+
+export default LikesSection
