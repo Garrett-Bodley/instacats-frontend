53d01df8d571c62607e47534073696c4177da48e
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-09-03 22:51:46 -0400

    Create separate styled Section component.

---
+++ src/Home/Card/Section.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  text-align: left;
+  font-size: 14px;
+  font-weight: 400;
+  padding: 0px 16px 0px 16px;
+  margin-bottom: 8px;
+`;
+
+export default Section;
