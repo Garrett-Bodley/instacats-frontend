9589e502b92973b598f6e1a7ba1a02cb990d5140
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-09-05 14:44:25 -0400

    Add styled anchor tag with active and hover states.

--- src/Home/Card/LikesSection.js
+++ src/Home/Card/LikesSection.js
@@ -5,6 +5,18 @@ import Section from "./Section";
 const Bold = styled.span`
   font-size: 14px;
   font-weight: 600;
+  cursor: pointer;
+`;
+
+const Anchor = styled.a`
+  color: rgb(38, 38, 38);
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+  &:active {
+    opacity: 0.5;
+  }
 `;
 
 const LikesSection = ({ likes }) => {
@@ -21,7 +33,8 @@ const LikesSection = ({ likes }) => {
     } else if (likes.length > 1) {
       return (
         <span>
-          Liked by <Bold>{`${likes[0].user.username}`}</Bold> and{" "}
+          Liked by{" "}
+          <Bold>{<Anchor href="#">{likes[0].user.username}</Anchor>}</Bold> and{" "}
           <Bold>others</Bold>
         </span>
       );
