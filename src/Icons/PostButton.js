782dff0b1eb9a37bb4ea9aea40196acfbefa1af0
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-09-13 14:09:02 -0400

    Make PostButton Icon.

---
+++ src/Icons/PostButton.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const DisabledPostButton = styled.button`
+  color: #0095f6;
+  pointer-events: none;
+  background: 0 0;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 18px;
+  padding: 0;
+  margin: 0;
+  border: 0;
+`;
+
+export const EnabledPostButton = styled.button`
+  color: #0095f6;
+  cursor: pointer;
+  background: 0 0;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 18px;
+  padding: 0;
+  margin: 0;
+  border: 0;
+`;
